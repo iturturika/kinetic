f017ac10eb783040a6bd1b3f016bc5709ffc6c96
ozzy <iturturika89@gmail.com>
2023-10-05 19:13:31 +0300

    fix item title 20 words...

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -7,7 +7,7 @@ const Item = ({setOnClickItem, id, title, price, image, label}) => {
   return (
     <div className='item' onClick={() => {setOnClickItem(id); bodyElement.style.overflowY = 'hidden';}}>
       <img src={image} alt='itemImg' className='item-image' draggable="false"></img>
-      <h3 className='item-title'>{title.substring(0, 30)}...</h3>
+      <h3 className='item-title'>{title.substring(0, 20)}...</h3>
       <p className='item-price'>{price}mdl</p>
       {/* {
         label === 'LEGIT' ? <p style={{fontSize: 16, color: "#3ee63e", marginTop: 5, borderRadius: 15, border: '2px solid #3ee63e', width: 80}}>Legit</p> : null
